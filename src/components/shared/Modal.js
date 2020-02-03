6c17e960d276aa6af8865717ceb5cbb467b7ebe0
Steven G <estengrove99@gmail.com>
2020-02-03 16:42:31 -0700

    SUBJECT: Updated <Modal/> & <ModalSM/> with useOutsideClick. Added deps.
     On branch react
     Your branch is up to date with 'origin/react'.
    
     Changes to be committed:
    	new file:   package 2.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/components/shared/Modal.js
    	modified:   src/components/shared/ModalSM.js

--- src/components/shared/Modal.js
+++ src/components/shared/Modal.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { PropTypes } from "prop-types";
+import { useOutsideClick } from "../../utils/useOutsideClick.js";
 import styles from "../../css/shared/Modal.module.scss";
 import sprite from "../../assets/buttons.svg";
 
 const Modal = ({ title, closeModal, children }) => {
+  const modalRef = useRef();
+  const { isOutside } = useOutsideClick(modalSmRef);
+
+  useEffect(() => {
+    let isMounted = true;
+    if (!isMounted) {
+      return;
+    }
+    if (isOutside) {
+      return closeModal();
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, [isOutside, closeModal]);
+
   return (
-    <aside className={styles.Modal}>
+    <aside className={styles.Modal} ref={modalRef}>
       <section className={styles.Modal_top}>
         <h4 className={styles.Modal_top_title}>{title}</h4>
         <svg className={styles.Modal_top_icon} onClick={closeModal}>
--- src/components/shared/ModalSM.js
+++ src/components/shared/ModalSM.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { PropTypes } from "prop-types";
+import { useOutsideClick } from "../../utils/useOutsideClick.js";
 import styles from "../../css/shared/ModalSM.module.scss";
 import sprite from "../../assets/buttons.svg";
 
 const ModalSM = ({ title, closeModal, children }) => {
+  const modalSMRef = useRef();
+  const { isOutside } = useOutsideClick(modalSmRef);
+
+  useEffect(() => {
+    let isMounted = true;
+    if (!isMounted) {
+      return;
+    }
+    if (isOutside) {
+      return closeModal();
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, [isOutside, closeModal]);
+
   return (
-    <aside className={styles.ModalSM}>
+    <aside className={styles.ModalSM} ref={modalSMRef}>
       <header className={styles.ModalSM_header}>
         <h2 className={styles.ModalSM_header_title}>{title}</h2>
         <svg className={styles.ModalSM_header_closeIcon} onClick={closeModal}>
