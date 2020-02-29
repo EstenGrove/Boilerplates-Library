78b9c18eb9d7be77748a4d04e85b0244d13f0dd1
Steven G <41505038+EstenGrove@users.noreply.github.com>
2020-02-29 13:35:13 -0700

    Create Template.test.js

---
+++ __tests__/Template.test.js
@@ -0,0 +1,7 @@
+import { isEmptyVal, isEmptyObj, EmptyArray } from '../helpers/utils_types';
+
+describe("Test Template", () => {
+	test("Testing Action", () => {
+		console.log("Testing");
+	});
+});
