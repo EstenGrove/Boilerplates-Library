0acf2d8f272304e68ec2ff15aa39555ff5bc6435
Steven G <41505038+EstenGrove@users.noreply.github.com>
2020-04-26 08:30:16 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
+	const user = process.env.API_USER; // using ENV vars
+	
 	console.log(`✅ - APP is listening on port ${PORT}`);
+	console.log(`API_USER: ${user}`);
+	
 });
 
